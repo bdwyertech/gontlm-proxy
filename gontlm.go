a90ccd83332624dccd696d184a5693d84319e541
Brian Dwyer <Brian.Dwyer@broadridge.com>
2019-10-06 13:06:25 -0400

    Leverage bhendo/concord as a RoundTripper
    Signed-off-by: Brian Dwyer <Brian.Dwyer@broadridge.com>

--- gontlm.go
+++ gontlm.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"crypto/tls"
+	"github.com/bhendo/concord"
+	"github.com/bhendo/concord/handshakers"
 	"github.com/elazarl/goproxy"
 	"golang.org/x/sys/windows/registry"
 	"log"
-	"net"
 	"net/http"
+	"net/url"
 	"regexp"
-	"time"
 )
 
 func main() {
@@ -25,71 +25,35 @@ func main() {
 		log.Fatal(err)
 	}
 	log.Printf("Forwarding Proxy is: %q\n", proxyServer)
-
+	proxyUrl, err := url.Parse("http://" + proxyServer)
+	if err != nil {
+		log.Fatal(err)
+	}
 	setGoProxyCA()
 	proxy := goproxy.NewProxyHttpServer()
-	proxy.Verbose = true
-
-	dialer := &net.Dialer{
-		Timeout:   30 * time.Second,
-		KeepAlive: 30 * time.Second,
-	}
+	proxy.Verbose = false
 
 	var AlwaysMitmAuth goproxy.FuncHttpsHandler = func(host string, ctx *goproxy.ProxyCtx) (*goproxy.ConnectAction, string) {
-		ntlmDialContext := WrapDialContext(dialer.DialContext, proxyServer)
-
-		proxy.Tr = &http.Transport{
-			Proxy:       nil,
-			Dial:        dialer.Dial,
-			DialContext: ntlmDialContext,
-			// TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-		}
-
 		return goproxy.MitmConnect, host
 	}
-
-	// Handle HTTP authenticate responses
-	//	proxy.OnResponse(HasNegotiateChallenge()).DoFunc(func(r *http.Response, ctx *goproxy.ProxyCtx) *http.Response {
-	//		ctx.Logf("Received 407 and Proxy-Authenticate from server, proceeding to reply")
-	//
-	//		headerstr := getAuthorizationHeader(os.Args[1])
-	//
-	//		// Modify the original request, and rerun the request
-	//		ctx.Req.Header["Proxy-Authorization"] = []string{headerstr}
-	//		client := http.Client{
-	//			Transport: proxy.Tr,
-	//		}
-	//
-	//		newr, err := client.Do(ctx.Req)
-	//
-	//		if err != nil {
-	//			ctx.Warnf("New request failed: %v", err)
-	//		}
-	//
-	//		ctx.Logf("Got response, forwarding it back to client")
-	//
-	//		// Return the new response in place of the original
-	//		return newr
-	//	})
-	//
 
 	// Handle HTTPS Connect Requests
 	proxy.OnRequest(goproxy.ReqHostMatches(regexp.MustCompile("^.*"))).HandleConnect(AlwaysMitmAuth)
 
 	// Handle HTTP Connect Requests
-	proxy.OnRequest(goproxy.Not(goproxy.ReqHostMatches(regexp.MustCompile("^.*:443$")))).
-		DoFunc(func(req *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
-			ntlmDialContext := WrapDialContext(dialer.DialContext, proxyServer)
+	proxy.OnRequest().DoFunc(func(req *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
+		t := concord.Transport{
+			Proxy:           http.ProxyURL(proxyUrl),
+			ProxyAuthorizer: &handshakers.NTLMProxyAuthorizer{},
+		}
 
-			proxy.Tr = &http.Transport{
-				Proxy:           nil,
-				Dial:            dialer.Dial,
-				DialContext:     ntlmDialContext,
-				TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-			}
-
-			return req, nil
+		ctx.RoundTripper = goproxy.RoundTripperFunc(func(req *http.Request, ctx *goproxy.ProxyCtx) (resp *http.Response, err error) {
+			resp, err = t.RoundTrip(req)
+			return
 		})
+
+		return req, nil
+	})
 
 	log.Fatal(http.ListenAndServe(":53128", proxy))
 
