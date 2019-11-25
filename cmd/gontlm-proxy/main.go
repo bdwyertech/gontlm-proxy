14584d00befed9a9aba70a49b131dca7a270c692
Brian Dwyer <Brian.Dwyer@broadridge.com>
2019-11-24 21:16:42 -0500

    Add version information

--- cmd/gontlm-proxy/main.go
+++ cmd/gontlm-proxy/main.go
@@ -1,9 +1,33 @@
 package main
 
 import (
+	"fmt"
 	proxy "github.com/bdwyertech/gontlm-proxy/pkg/gontlm-proxy"
 )
 
+import "flag"
+
+var verFlag = flag.Bool("version", false, "Display version")
+
+var GitCommit string
+var ReleaseVer string
+
+func showVersion() {
+	if GitCommit == "" {
+		GitCommit = "DEVELOPMENT"
+	}
+	if ReleaseVer == "" {
+		ReleaseVer = "DEVELOPMENT"
+	}
+	fmt.Println("version:", ReleaseVer)
+	fmt.Println("commit:", GitCommit)
+}
+
 func main() {
+	flag.Parse()
+	if *verFlag {
+		showVersion()
+		return
+	}
 	proxy.Run()
 }
